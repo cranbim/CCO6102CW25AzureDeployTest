a514d7ea668db6c0985e6e845baf6e31c8e32d5c
Dave Webb <dave.webb@cranbim.com>
2025-12-12 15:22:41 +0000

    simpler test

--- models/posts.js
+++ models/posts.js
@@ -1,61 +1,31 @@
-// let nextPostID=3
+let nextPostID=2
 
-// let postData=[
-//     {
-//         postid: 0,
-//         message:"Hi it's Dave",
-//         user:"Dave"
-//     },{
-//         postid: 1,
-//         message:"Glad it's Thursday",
-//         user:"Julie"
-//     },{
-//         postid: 2,
-//         message:"I am a post",
-//         user:"Noodle"
-//     }
-// ]
-
-const mongoose=require('mongoose')
-
-const {Schema, model} = mongoose
-
-const postSchema=new Schema({
-    user: String,
-    message: String,
-    likes: Number,
-    time: Date
-})
-
-const postData=model('postsDave', postSchema)
+let postData=[
+    {
+        postid: 0,
+        message:"Hi it's Dave",
+        user:"Dave"
+    },{
+        postid: 1,
+        message:"Glad it's Thursday",
+        user:"Julie"
+    }
+]
 
 function getPosts(){
     return postData.slice()
 }
 
-async function getLatestNPosts(n=3){
-    // return postData.slice(-n).reverse()
-    let foundData=[]
-    foundData=await postData.find({}).sort({'time':-1}).limit(n).exec()
-    return foundData
-}
-
 function addPost(message, user){
     let newPost={
-        // postid: nextPostID++,
+        postid: nextPostID++,
         message: message,
-        user: user,
-        likes: 0,
-        time: Date.now()
+        user: user
     }
-    postData.create(newPost)
-    .catch(err=>{
-        console.log("Error",err)
-    })
+    postData.push(newPost)
 }
 
 module.exports={
     getPosts,
-    addPost,
-    getLatestNPosts
+    addPost
 }
--- models/users.js
+++ models/users.js
@@ -1,35 +1,29 @@
+let userData=[
+    {
+        username: "user1",
+        password: "123"
+    },{
+        username: "user2",
+        password: "456"
+    }
+]
 
-const mongoose=require('mongoose')
-
-const {Schema, model} = mongoose
-
-const userSchema=new Schema({
-    username: String,
-    password: String,
-})
-
-const userData=model('user', userSchema)
-
-async function addUser(usernameFromForm, password){
-    // let found=userData.find(thisUser=>thisUser.username==username)
-    let found=null
-    found=await userData.findOne({username:usernameFromForm}).exec()
+function addUser(username, password){
+    let found=userData.find(thisUser=>thisUser.username==username)
     if(found){
         return false
     } else {
         let newUser={
-            username:usernameFromForm,
+            username:username,
             password:password
         }
-        await userData.create(newUser)
+        userData.push(newUser)
         return true
     }
 }
 
-async function checkUser(usernameFromForm, password){
-    // let found=userData.find(thisUser=>thisUser.username==username)
-    let found=null
-    found=await userData.findOne({username:usernameFromForm}).exec()
+function checkUser(username, password){
+    let found=userData.find(thisUser=>thisUser.username==username)
     if(found){
         return found.password==password
     } else {
